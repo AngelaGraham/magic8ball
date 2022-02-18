281418d94431aff158c998ea843349a171dd00ef
Angela Graham <angelagraham101@gmail.com>
2022-02-17 16:24:48 -0800

    init commit still confused

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// const answer = document.getElementById('answers')
+//
+// askMe.addEventLister('click', ()=> {
+
+//     let prompt = ("enter your question");
+  
+
+// })
+
+// const answers = random_image 
+// ("images/magic8ball_1.png", 
+// "images/magic8ball_2.png",
+// "images/magic8ball_3.png",
+// "images/magic8ball_4.png",
+// "images/magic8ball_5.png",
+// "images/magic8ball_6.png",
+// "images/magic8ball_7.png",
+// "images/magic8ball_8.png",
+// "images/magic8ball_9.png",
+// "images/magic8ball_10.png",
+// "images/magic8ball_11.png",
+// "images/magic8ball_12.png",
+// "images/magic8ball_13.png",
+// "images/magic8ball_14.png",
+// "images/magic8ball_15.png",
+// "images/magic8ball_16.png",
+// "images/magic8ball_17.png",
+// "images/magic8ball_18.png",
+// "images/magic8ball_19.png",
+// "images/magic8ball_20.png"
+// )
+// function get_random_image(){
+// let randomNumber = Math.floor(Math.random()*random_image.length);
+// }
+
+// let num = Math.floor(Math.random() * Math.floor(answers.length));
+//            answer.innerText = answers[num];
---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+
+//  const answers = answers_image 
+//   ("images/magic8ball_1.png", 
+//   "images/magic8ball_2.png",
+//  "images/magic8ball_3.png",
+//  "images/magic8ball_4.png",
+//  "images/magic8ball_5.png",
+//  "images/magic8ball_6.png",
+//  "images/magic8ball_7.png",
+//  "images/magic8ball_8.png",
+//  "images/magic8ball_9.png",
+//  "images/magic8ball_10.png",
+//  "images/magic8ball_11.png",
+//  "images/magic8ball_12.png",
+//  "images/magic8ball_13.png",
+//  "images/magic8ball_14.png",
+//  "images/magic8ball_15.png",
+//  "images/magic8ball_16.png",
+//  "images/magic8ball_17.png",
+//  "images/magic8ball_18.png",
+//  "images/magic8ball_19.png",
+//  "images/magic8ball_20.png"
+//  )
+ 
+
+
+
+ function get_answers_image(){
+
+    let answers =
+    ["images/magic8ball_1.png", 
+    "images/magic8ball_2.png",
+    "images/magic8ball_3.png",
+    "images/magic8ball_4.png",
+    "images/magic8ball_5.png",
+    "images/magic8ball_6.png",
+    "images/magic8ball_7.png",
+    "images/magic8ball_8.png",
+    "images/magic8ball_9.png",
+    "images/magic8ball_10.png",
+    "images/magic8ball_11.png",
+    "images/magic8ball_12.png",
+    "images/magic8ball_13.png",
+    "images/magic8ball_14.png",
+    "images/magic8ball_15.png",
+    "images/magic8ball_16.png",
+    "images/magic8ball_17.png",
+    "images/magic8ball_18.png",
+    "images/magic8ball_19.png",
+    "images/magic8ball_20.png"]
+
+    const yourInput = prompt ("Ask me anything");
+
+
+     random_index = Math.floor(Math.random()*answers_image.length);
+      selected_image = random_image[random_index]
+      document.getElementById('answers')
+     .src=`./image/${selected_image}`  
+
+    const nextQuestion = prompt ("Next question please")
+  } 
